6c22053870df8597686bac253f6e41fc14871228
Matt Greeley <greeleymp@gmail.com>
2023-01-16 22:09:28 -0600

    move api key

--- src/Hooks/FirebaseApp.ts
+++ src/Hooks/FirebaseApp.ts
@@ -1,15 +1,16 @@
-import { getDatabase } from 'firebase/database';
+import { getDatabase } from "firebase/database";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
+  apiKey: "<api key here>",
   authDomain: "trivia-c9a1d.firebaseapp.com",
   databaseURL: "https://trivia-c9a1d-default-rtdb.firebaseio.com",
   projectId: "trivia-c9a1d",
   storageBucket: "trivia-c9a1d.appspot.com",
   messagingSenderId: "259033362478",
   appId: "1:259033362478:web:b616e6a3e1522a10d22fbf",
-  measurementId: "G-ZXY8HSMBSL"
+  measurementId: "G-ZXY8HSMBSL",
 };
 
 // Initialize Firebase
